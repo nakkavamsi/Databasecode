f11b037003c8c0b5efcae279b57d3158cf503383
Vamsi Nakka <nakka.krishnavamsi@gmail.com>
2026-05-22 11:48:49 -0500

    created db project

---
+++ dbo/StoredProcedures/usp_GetCustomerById.sql
@@ -0,0 +1,14 @@
+CREATE PROCEDURE [dbo].[usp_GetCustomerById]
+    @CustomerId INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT
+        [CustomerId],
+        [Name],
+        [Email],
+        [CreatedAt]
+    FROM [dbo].[Customer]
+    WHERE [CustomerId] = @CustomerId;
+END;
